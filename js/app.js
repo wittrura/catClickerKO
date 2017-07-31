2495977c72648fc44da1124c7ba518e6ac710c6f
Ryan Wittrup <ryan.wittrup@gmail.com>
2017-07-31 16:35:50 -0500

    Moves cats to separate var, adds list feature to change active cat

--- js/app.js
+++ js/app.js
@@ -1,3 +1,48 @@
+var initialCats = [
+  {
+    name: 'Baxter',
+    clickCount: 0,
+    imgSrc: 'img/cat-clicker-1-small.jpg',
+    imgAttribution: 'https://www.flicker.com',
+    nicknames: [
+      'Mittens',
+      'Bruiser',
+      'Gadnuk Breaker of Worlds'
+    ]
+  },
+  {
+    name: 'Mittens',
+    clickCount: 0,
+    imgSrc: 'img/cat-clicker-2-small.jpg',
+    imgAttribution: 'https://www.flicker.com',
+    nicknames: [
+      'Mitzy',
+      'M Dog',
+    ]
+  },
+  {
+    name: 'Tabby',
+    clickCount: 0,
+    imgSrc: 'img/cat-clicker-3-small.jpg',
+    imgAttribution: 'https://www.flicker.com',
+    nicknames: [
+      'T-Train',
+      'T Money',
+      'T Pain'
+    ]
+  },
+  {
+    name: 'Bruiser',
+    clickCount: 0,
+    imgSrc: 'img/cat-clicker-4-small.jpeg',
+    imgAttribution: 'https://www.flicker.com',
+    nicknames: [
+      'Brews',
+      'Brutus',
+    ]
+  }
+];
+
 var Cat = function (data) {
   this.clickCount = ko.observable(data.clickCount);
   this.name = ko.observable(data.name);
@@ -21,23 +66,21 @@ var Cat = function (data) {
 var ViewModel = function () {
   var self = this;
 
-  this.currentCat = ko.observable(new Cat(
-    {
-      clickCount: 0,
-      name: 'Tabby',
-      imgSrc: 'img/cat-clicker-1-small.jpg',
-      imgAttribution: 'https://www.flicker.com',
-      nicknames: [
-        'Mittens',
-        'Bruiser',
-        'Gadnuk Breaker of Worlds'
-      ]
-    }
-  ));
+  this.catList = ko.observableArray([]);
+
+  initialCats.forEach(function(cat){
+    self.catList.push( new Cat(cat) );
+  });
+
+  this.currentCat = ko.observable(this.catList()[0]);
 
   this.incrementCounter = function() {
     self.currentCat().clickCount(self.currentCat().clickCount() + 1);
   };
+
+  this.makeActiveCat = function(cat) {
+    self.currentCat(cat);
+  }
 }
 
 ko.applyBindings(new ViewModel());
